159eb0283c54c7d03bdf28254daed6eac874074c
Ariel Coppes <ariel.coppes@gemserk.com>
2011-06-23 15:01:05 -0300

    modified game to hold super sheep prototype

--- discovertheway-core/src/main/java/com/gemserk/games/discovertheway/Game.java
+++ discovertheway-core/src/main/java/com/gemserk/games/discovertheway/Game.java
@@ -1,95 +1,96 @@
 package com.gemserk.games.discovertheway;
 
-import java.text.MessageFormat;
-
-import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.World;
 import com.gemserk.animation4j.converters.Converters;
 import com.gemserk.animation4j.gdx.converters.LibgdxConverters;
 import com.gemserk.commons.gdx.GameStateImpl;
 import com.gemserk.commons.gdx.ScreenImpl;
+import com.gemserk.commons.gdx.box2d.BodyBuilder;
 import com.gemserk.commons.gdx.camera.Libgdx2dCamera;
 import com.gemserk.commons.gdx.camera.Libgdx2dCameraTransformImpl;
+import com.gemserk.commons.gdx.graphics.ImmediateModeRendererUtils;
+import com.gemserk.commons.gdx.graphics.SpriteBatchUtils;
 
 public class Game extends com.gemserk.commons.gdx.Game {
 
-	class AccelerometerTest extends GameStateImpl {
+	class SuperSheepGameState extends GameStateImpl {
 
 		private SpriteBatch spriteBatch;
-		// private Texture whiteRectangle;
-
-		private Vector2 position;
-		private Vector2 velocity;
-		private Vector2 acceleration;
-
 		private Libgdx2dCamera camera;
 		private Sprite whiteRectangleSprite;
+		private World world;
+		private Body body;
+		private Vector2 direction;
 
 		@Override
 		public void init() {
 			spriteBatch = new SpriteBatch();
-			position = new Vector2();
-			velocity = new Vector2();
-			acceleration = new Vector2();
 			camera = new Libgdx2dCameraTransformImpl();
-			camera.zoom(16f);
+			world = new World(new Vector2(), false);
+
+			camera.zoom(32f);
 
 			Texture whiteRectangle = new Texture(Gdx.files.internal("data/images/white-rectangle.png"));
 			whiteRectangleSprite = new Sprite(whiteRectangle);
-			whiteRectangleSprite.setSize(2f, 2f);
+			whiteRectangleSprite.setSize(1f, 1f);
+
+			BodyBuilder bodyBuilder = new BodyBuilder(world);
+			body = bodyBuilder.mass(50f) //
+					.boxShape(0.5f, 0.5f) //
+					.position(0f, 0f) //
+					.restitution(0f) //
+					.type(BodyType.DynamicBody) //
+					.build();
+			direction = new Vector2(1f, 0f);
 		}
 
 		@Override
 		public void render(int delta) {
-			Gdx.app.log("Discover The Name", MessageFormat.format("accelerometer - {0},{1},{2}", Gdx.input.getAccelerometerX(), Gdx.input.getAccelerometerY(), Gdx.input.getAccelerometerZ()));
-			Gdx.app.log("Discover The Name", MessageFormat.format("rotation - {0},{1},{2}", Gdx.input.getPitch(), Gdx.input.getRoll(), Gdx.input.getAzimuth()));
-			
+			Vector2 position = body.getTransform().getPosition();
+
 			Gdx.graphics.getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
 			camera.apply(spriteBatch);
 			spriteBatch.begin();
-			// spriteBatch.draw(whiteRectangle, position.x, position.y);
-			whiteRectangleSprite.draw(spriteBatch);
+			SpriteBatchUtils.drawCentered(spriteBatch, whiteRectangleSprite, position.x, position.y, body.getAngle() * MathUtils.radiansToDegrees);
 			spriteBatch.end();
+
+			float x = position.x + direction.tmp().mul(2f).x;
+			float y = position.y + direction.tmp().mul(2f).y;
+
+			ImmediateModeRendererUtils.drawLine(position.x, position.y, x, y, Color.GREEN);
 		}
 
 		@Override
 		public void update(int delta) {
+			world.step(Gdx.app.getGraphics().getDeltaTime(), 3, 3);
 
-			if (Gdx.app.getType() == ApplicationType.Android) {
-				acceleration.x = -Gdx.input.getAccelerometerX();
-				acceleration.y = -Gdx.input.getAccelerometerY();
-			} 
+			Vector2 position = body.getTransform().getPosition();
 
-			velocity.x += acceleration.x * 0.001f * delta;
-			velocity.y += acceleration.y * 0.001f * delta;
+			if (Gdx.input.isKeyPressed(Keys.LEFT))
+				direction.rotate(180f * delta * 0.001f);
+			else if (Gdx.input.isKeyPressed(Keys.RIGHT))
+				direction.rotate(-180f * delta * 0.001f);
 
-			position.x += velocity.x * 0.001f * delta;
-			position.y += velocity.y * 0.001f * delta;
+			body.setTransform(position, direction.angle() * MathUtils.degreesToRadians);
+			body.applyForce(direction.tmp().mul(5000f), position);
 
-			if (position.x > Gdx.graphics.getWidth() - whiteRectangleSprite.getWidth()) {
-				position.x = Gdx.graphics.getWidth() - whiteRectangleSprite.getWidth();
-				velocity.x = 0f;
-			}
-
-			if (position.x < 0f) {
-				position.x = 0f;
-				velocity.x = 0f;
-			}
-
-			if (position.y > Gdx.graphics.getHeight() - whiteRectangleSprite.getHeight()) {
-				position.y = Gdx.graphics.getHeight() - whiteRectangleSprite.getHeight();
-				velocity.y = 0f;
-			}
-
-			if (position.y < 0f) {
-				position.y = 0f;
-				velocity.y = 0f;
+			Vector2 linearVelocity = body.getLinearVelocity();
+			float speed = linearVelocity.len();
+			float maxSpeed = 5f;
+			if (speed > maxSpeed) {
+				linearVelocity.mul(maxSpeed / speed);
+				body.setLinearVelocity(linearVelocity);
 			}
 
 			whiteRectangleSprite.setPosition(position.x, position.y);
@@ -102,14 +103,14 @@ public class Game extends com.gemserk.commons.gdx.Game {
 
 	}
 
-	private AccelerometerTest accelerometerTestGameState;
+	private SuperSheepGameState accelerometerTestGameState;
 
 	@Override
 	public void create() {
 		Converters.register(Vector2.class, LibgdxConverters.vector2());
 		Converters.register(Color.class, LibgdxConverters.color());
 
-		accelerometerTestGameState = new AccelerometerTest();
+		accelerometerTestGameState = new SuperSheepGameState();
 
 		setScreen(new ScreenImpl(accelerometerTestGameState));
 	}
