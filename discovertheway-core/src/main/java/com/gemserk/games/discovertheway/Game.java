00fbdb69d93be628086d39b3c7ced07050b43ecb
Ariel Coppes <ariel.coppes@gemserk.com>
2011-06-23 15:46:43 -0300

    modified some values to make it similar to original super sheep and added android controls

--- discovertheway-core/src/main/java/com/gemserk/games/discovertheway/Game.java
+++ discovertheway-core/src/main/java/com/gemserk/games/discovertheway/Game.java
@@ -1,5 +1,6 @@
 package com.gemserk.games.discovertheway;
 
+import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
@@ -17,6 +18,7 @@ import com.gemserk.animation4j.gdx.converters.LibgdxConverters;
 import com.gemserk.commons.gdx.GameStateImpl;
 import com.gemserk.commons.gdx.ScreenImpl;
 import com.gemserk.commons.gdx.box2d.BodyBuilder;
+import com.gemserk.commons.gdx.box2d.Box2DCustomDebugRenderer;
 import com.gemserk.commons.gdx.camera.Libgdx2dCamera;
 import com.gemserk.commons.gdx.camera.Libgdx2dCameraTransformImpl;
 import com.gemserk.commons.gdx.graphics.ImmediateModeRendererUtils;
@@ -32,6 +34,7 @@ public class Game extends com.gemserk.commons.gdx.Game {
 		private World world;
 		private Body body;
 		private Vector2 direction;
+		private Box2DCustomDebugRenderer box2dCustomDebugRenderer;
 
 		@Override
 		public void init() {
@@ -41,17 +44,34 @@ public class Game extends com.gemserk.commons.gdx.Game {
 
 			camera.zoom(32f);
 
+			box2dCustomDebugRenderer = new Box2DCustomDebugRenderer((Libgdx2dCameraTransformImpl) camera, world);
+
 			Texture whiteRectangle = new Texture(Gdx.files.internal("data/images/white-rectangle.png"));
 			whiteRectangleSprite = new Sprite(whiteRectangle);
-			whiteRectangleSprite.setSize(1f, 1f);
+			whiteRectangleSprite.setSize(0.5f, 0.5f);
 
 			BodyBuilder bodyBuilder = new BodyBuilder(world);
 			body = bodyBuilder.mass(50f) //
-					.boxShape(0.5f, 0.5f) //
-					.position(0f, 0f) //
+					.boxShape(0.25f, 0.25f) //
+					.position(0f, 5f) //
 					.restitution(0f) //
 					.type(BodyType.DynamicBody) //
 					.build();
+
+			Body obstacle1 = bodyBuilder.mass(1000f) //
+					.polygonShape(new Vector2[] { new Vector2(3f, 1.5f), new Vector2(1f, 4f), new Vector2(-2.5f, 1f), new Vector2(-1.5f, -2.5f), new Vector2(1f, -1.5f), }) //
+					.position(15f, 5f) //
+					.restitution(0f) //
+					.type(BodyType.StaticBody) //
+					.build();
+
+			Body obstacle2 = bodyBuilder.mass(1000f) //
+					.polygonShape(new Vector2[] { new Vector2(3f, 1.5f), new Vector2(1f, 4f), new Vector2(-2.5f, 1f), new Vector2(-1.5f, -2.5f), new Vector2(1f, -1.5f), }) //
+					.restitution(0f) //
+					.type(BodyType.StaticBody) //
+					.build();
+			obstacle2.setTransform(new Vector2(12f, 11f), 90f);
+
 			direction = new Vector2(1f, 0f);
 		}
 
@@ -65,10 +85,12 @@ public class Game extends com.gemserk.commons.gdx.Game {
 			SpriteBatchUtils.drawCentered(spriteBatch, whiteRectangleSprite, position.x, position.y, body.getAngle() * MathUtils.radiansToDegrees);
 			spriteBatch.end();
 
-			float x = position.x + direction.tmp().mul(2f).x;
-			float y = position.y + direction.tmp().mul(2f).y;
+			float x = position.x + direction.tmp().mul(1f).x;
+			float y = position.y + direction.tmp().mul(1f).y;
 
 			ImmediateModeRendererUtils.drawLine(position.x, position.y, x, y, Color.GREEN);
+
+			box2dCustomDebugRenderer.render();
 		}
 
 		@Override
@@ -77,17 +99,33 @@ public class Game extends com.gemserk.commons.gdx.Game {
 
 			Vector2 position = body.getTransform().getPosition();
 
-			if (Gdx.input.isKeyPressed(Keys.LEFT))
-				direction.rotate(180f * delta * 0.001f);
-			else if (Gdx.input.isKeyPressed(Keys.RIGHT))
-				direction.rotate(-180f * delta * 0.001f);
+			if (Gdx.app.getType() == ApplicationType.Android) {
+
+				if (Gdx.input.isTouched()) {
+
+					float x = Gdx.input.getX();
+
+					if (x < Gdx.graphics.getWidth() / 2) {
+						direction.rotate(360f * delta * 0.001f);
+					} else {
+						direction.rotate(-360f * delta * 0.001f);
+					}
+
+				}
+
+			} else {
+				if (Gdx.input.isKeyPressed(Keys.LEFT))
+					direction.rotate(360f * delta * 0.001f);
+				else if (Gdx.input.isKeyPressed(Keys.RIGHT))
+					direction.rotate(-360f * delta * 0.001f);
+			}
 
 			body.setTransform(position, direction.angle() * MathUtils.degreesToRadians);
 			body.applyForce(direction.tmp().mul(5000f), position);
 
 			Vector2 linearVelocity = body.getLinearVelocity();
 			float speed = linearVelocity.len();
-			float maxSpeed = 5f;
+			float maxSpeed = 7f;
 			if (speed > maxSpeed) {
 				linearVelocity.mul(maxSpeed / speed);
 				body.setLinearVelocity(linearVelocity);
@@ -103,16 +141,20 @@ public class Game extends com.gemserk.commons.gdx.Game {
 
 	}
 
-	private SuperSheepGameState accelerometerTestGameState;
-
 	@Override
 	public void create() {
 		Converters.register(Vector2.class, LibgdxConverters.vector2());
 		Converters.register(Color.class, LibgdxConverters.color());
+		setScreen(new ScreenImpl(new SuperSheepGameState()));
+	}
 
-		accelerometerTestGameState = new SuperSheepGameState();
-
-		setScreen(new ScreenImpl(accelerometerTestGameState));
+	@Override
+	public void render() {
+		super.render();
+		if (Gdx.input.isKeyPressed(Keys.R)) {
+			getScreen().dispose();
+			setScreen(new ScreenImpl(new SuperSheepGameState()));
+		}
 	}
 
 }
