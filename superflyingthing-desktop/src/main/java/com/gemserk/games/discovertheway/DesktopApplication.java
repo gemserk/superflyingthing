fbdb0a4804ae5e15545420580e5fc68bd683fc32
Ariel Coppes <ariel.coppes@gemserk.com>
2011-06-27 11:58:54 -0300

    modified gitignore and changed DesktopApplication name

--- superflyingthing-desktop/src/main/java/com/gemserk/games/discovertheway/DesktopApplication.java
+++ superflyingthing-desktop/src/main/java/com/gemserk/games/discovertheway/DesktopApplication.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 public class DesktopApplication {
 
 	public static void main(String[] argv) {
-		new LwjglApplication(new Game(), "Discover The Way", 800, 480, false);
+		new LwjglApplication(new Game(), "Super Flying Thing Prototype", 800, 480, false);
 	}
 
 }
