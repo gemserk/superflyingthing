72c38876b868c80a0068c8beeaa647e9b98f1341
Ariel Coppes <ariel.coppes@gemserk.com>
2011-07-14 11:12:17 -0300

    added particle effect for explosion and a test on main menu screen

--- superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/resources/GameResources.java
+++ superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/resources/GameResources.java
@@ -7,7 +7,7 @@ import com.gemserk.resources.ResourceManager;
  * Declares all resources needed for the game.
  */
 public class GameResources extends LibgdxResourceBuilder {
-	
+
 	public static void load(ResourceManager<String> resourceManager) {
 		new GameResources(resourceManager);
 	}
@@ -19,29 +19,32 @@ public class GameResources extends LibgdxResourceBuilder {
 		texture("LwjglLogoTexture", "data/images/logo-lwjgl-512x256-inverted.png");
 		texture("LibgdxLogoTexture", "data/images/logo-libgdx-clockwork-512x256.png");
 		texture("WhiteRectangleTexture", "data/images/white-rectangle.png");
-		
+
 		sprite("GemserkLogo", "GemserkLogoTexture");
 		sprite("GemserkLogoBlur", "GemserkLogoTextureBlur");
 		sprite("LwjglLogo", "LwjglLogoTexture", 0, 0, 512, 185);
 		sprite("LibgdxLogo", "LibgdxLogoTexture", 0, 25, 512, 256 - 50);
 		sprite("WhiteRectangle", "WhiteRectangleTexture");
-		
+
 		font("FpsFont", "data/fonts/purisa-18.png", "data/fonts/purisa-18.fnt", false);
 		font("TitleFont", "data/fonts/purisa-18.png", "data/fonts/purisa-18.fnt", false);
 		font("ButtonFont", "data/fonts/purisa-18.png", "data/fonts/purisa-18.fnt", false);
 		font("GameFont", "data/fonts/purisa-18.png", "data/fonts/purisa-18.fnt", false);
 		font("LevelFont", "data/fonts/purisa-18-bold.png", "data/fonts/purisa-18-bold.fnt", false);
 		font("InstructionsFont", "data/fonts/purisa-18-bold.png", "data/fonts/purisa-18-bold.fnt", false);
-		
+
 		texture("PlanetTexture", "data/images/planet.png");
 		sprite("Planet", "PlanetTexture");
-		
+
 		texture("PlanetBlurTexture", "data/images/planet_blur.png");
 		sprite("PlanetBlur", "PlanetBlurTexture");
-		
+
 		texture("BackgroundTexture", "data/images/background01-1024x512.jpg", false);
 		sprite("BackgroundSprite", "BackgroundTexture");
 
+		particleEffect("ExplosionEffect", "data/particles/ExplosionEffect", "data/particles");
+		particleEmitter("ExplosionEmitter", "ExplosionEffect", "Explosion");
 	}
+
 
 }
