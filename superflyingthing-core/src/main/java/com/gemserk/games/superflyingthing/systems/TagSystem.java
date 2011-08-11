07e39a268ad189ac539ddbd6607c43db80729cde
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-11 16:38:28 -0300

    added fix to allow create new entity with already used tag and avoid removing it when deleting an entity with that tag

--- superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/systems/TagSystem.java
+++ superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/systems/TagSystem.java
@@ -6,36 +6,41 @@ import com.artemis.EntityProcessingSystem;
 import com.gemserk.games.superflyingthing.components.TagComponent;
 
 public class TagSystem extends EntityProcessingSystem {
-	
+
 	private ComponentMapper<TagComponent> tagComponentMapper;
 
 	public TagSystem() {
 		super(TagComponent.class);
 	}
-	
+
 	@Override
 	protected void initialize() {
 		super.initialize();
 		tagComponentMapper = new ComponentMapper<TagComponent>(TagComponent.class, world.getEntityManager());
 	}
-	
+
 	@Override
 	protected void added(Entity e) {
 		super.added(e);
 		TagComponent tagComponent = tagComponentMapper.get(e);
 		world.getTagManager().register(tagComponent.getTag(), e);
 	}
-	
+
 	@Override
 	protected void removed(Entity e) {
 		super.removed(e);
 		TagComponent tagComponent = tagComponentMapper.get(e);
+		Entity entityWithTag = world.getTagManager().getEntity(tagComponent.getTag());
+		if (entityWithTag == null)
+			return;
+		if (entityWithTag != e)
+			return;
 		world.getTagManager().unregister(tagComponent.getTag());
 	}
 
 	@Override
 	protected void process(Entity e) {
-		
+
 	}
 
 }
