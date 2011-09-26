c682f98adc678e5f640c9a5891486e296dbde28c
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-26 20:53:03 -0300

    modified ControllerSettings screen to use the same scene template

--- superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/gamestates/ControllerSettingsGameState.java
+++ superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/gamestates/ControllerSettingsGameState.java
@@ -9,21 +9,10 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.gemserk.analytics.Analytics;
 import com.gemserk.animation4j.transitions.sync.Synchronizers;
-import com.gemserk.commons.artemis.EntityBuilder;
 import com.gemserk.commons.artemis.WorldWrapper;
-import com.gemserk.commons.artemis.render.RenderLayers;
-import com.gemserk.commons.artemis.systems.RenderLayerSpriteBatchImpl;
-import com.gemserk.commons.artemis.systems.RenderableSystem;
-import com.gemserk.commons.artemis.systems.SpriteUpdateSystem;
-import com.gemserk.commons.artemis.templates.EntityFactory;
-import com.gemserk.commons.artemis.templates.EntityFactoryImpl;
 import com.gemserk.commons.gdx.GameStateImpl;
-import com.gemserk.commons.gdx.camera.Libgdx2dCamera;
-import com.gemserk.commons.gdx.camera.Libgdx2dCameraTransformImpl;
-import com.gemserk.commons.gdx.games.SpatialImpl;
 import com.gemserk.commons.gdx.graphics.SpriteUtils;
 import com.gemserk.commons.gdx.gui.ButtonHandler;
 import com.gemserk.commons.gdx.gui.Container;
@@ -33,15 +22,12 @@ import com.gemserk.commons.gdx.gui.Panel;
 import com.gemserk.commons.gdx.gui.TextButton;
 import com.gemserk.componentsengine.input.InputDevicesMonitorImpl;
 import com.gemserk.componentsengine.input.LibgdxInputMappingBuilder;
-import com.gemserk.componentsengine.utils.Parameters;
-import com.gemserk.componentsengine.utils.ParametersWrapper;
 import com.gemserk.games.superflyingthing.Colors;
 import com.gemserk.games.superflyingthing.Game;
-import com.gemserk.games.superflyingthing.Layers;
 import com.gemserk.games.superflyingthing.Screens;
 import com.gemserk.games.superflyingthing.preferences.PlayerProfile;
+import com.gemserk.games.superflyingthing.scenes.EmptySceneTemplate;
 import com.gemserk.games.superflyingthing.scripts.controllers.ControllerType;
-import com.gemserk.games.superflyingthing.templates.EntityTemplates;
 import com.gemserk.resources.ResourceManager;
 
 public class ControllerSettingsGameState extends GameStateImpl {
@@ -52,7 +38,7 @@ public class ControllerSettingsGameState extends GameStateImpl {
 		private final Color notOverColor = Color.WHITE;
 		private final Color overColor = Color.GREEN;
 		private final Sprite underlineSprite;
-		
+
 		private Control selectedControl;
 
 		public MultipleButtonControlWithUnderline(Sprite underlineSprite) {
@@ -100,16 +86,16 @@ public class ControllerSettingsGameState extends GameStateImpl {
 			}
 
 			textButton.setNotOverColor(selectedColor);
-			
+
 			selectedControl = textButton;
-			
+
 		}
 
 		@Override
 		public void draw(SpriteBatch spriteBatch) {
 			super.draw(spriteBatch);
 			underlineSprite.draw(spriteBatch);
-			
+
 			SpriteUtils.centerOn(underlineSprite, selectedControl.getX(), selectedControl.getY() - 15f, 0f, 0.5f);
 		}
 
@@ -129,7 +115,7 @@ public class ControllerSettingsGameState extends GameStateImpl {
 	private InputDevicesMonitorImpl<String> inputDevicesMonitor;
 
 	private Container container;
-	private RenderLayers renderLayers;
+	// private RenderLayers renderLayers;
 	private WorldWrapper worldWrapper;
 
 	private ControllerType selectedControllerType;
@@ -250,47 +236,6 @@ public class ControllerSettingsGameState extends GameStateImpl {
 
 		container.add(panel);
 
-		renderLayers = new RenderLayers();
-
-		final Libgdx2dCamera backgroundLayerCamera = new Libgdx2dCameraTransformImpl();
-
-		renderLayers.add(Layers.FirstBackground, new RenderLayerSpriteBatchImpl(-10000, -500, backgroundLayerCamera, spriteBatch), game.getGamePreferences().isFirstBackgroundEnabled());
-		renderLayers.add(Layers.SecondBackground, new RenderLayerSpriteBatchImpl(-500, -100, backgroundLayerCamera, spriteBatch), game.getGamePreferences().isSecondBackgroundEnabled());
-
-		World world = new com.artemis.World();
-		worldWrapper = new WorldWrapper(world);
-		worldWrapper.addRenderSystem(new SpriteUpdateSystem());
-		worldWrapper.addRenderSystem(new RenderableSystem(renderLayers));
-		worldWrapper.init();
-
-		EntityFactory entityFactory = new EntityFactoryImpl(world);
-		Parameters parameters = new ParametersWrapper();
-
-		EntityTemplates entityTemplates = new EntityTemplates(null, world, resourceManager, new EntityBuilder(world), new EntityFactoryImpl(world), null);
-
-		entityFactory.instantiate(entityTemplates.getStaticSpriteTemplate(), parameters //
-				.put("color", Color.WHITE) //
-				.put("layer", (-999)) //
-				.put("spatial", new SpatialImpl(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0f)) //
-				.put("center", new Vector2(0f, 0f)) //
-				.put("spriteId", "BackgroundSprite") //
-				);
-
-		entityFactory.instantiate(entityTemplates.getStaticSpriteTemplate(), parameters //
-				.put("color", Color.GREEN) //
-				.put("layer", (-400)) //
-				.put("spatial", new SpatialImpl((Gdx.graphics.getWidth() * 0.57f), (Gdx.graphics.getHeight() * 0.23f), 160f, 160f, 86f)) //
-				.put("center", new Vector2(0.5f, 0.5f)) //
-				.put("spriteId", "FogSprite") //
-				);
-		entityFactory.instantiate(entityTemplates.getStaticSpriteTemplate(), parameters //
-				.put("color", Color.RED) //
-				.put("layer", (-400)) //
-				.put("spatial", new SpatialImpl((Gdx.graphics.getWidth() * 0.24f), (Gdx.graphics.getHeight() * 0.68f), 120f, 120f, 189f)) //
-				.put("center", new Vector2(0.5f, 0.5f)) //
-				.put("spriteId", "FogSprite") //
-				);
-
 		inputDevicesMonitor = new InputDevicesMonitorImpl<String>();
 		new LibgdxInputMappingBuilder<String>(inputDevicesMonitor, Gdx.input) {
 			{
@@ -299,6 +244,16 @@ public class ControllerSettingsGameState extends GameStateImpl {
 		};
 
 		Analytics.traker.trackPageView("/settings/start", "/settings/start", null);
+
+		worldWrapper = new WorldWrapper(new World());
+
+		EmptySceneTemplate emptySceneTemplate = new EmptySceneTemplate();
+		emptySceneTemplate.setResourceManager(resourceManager);
+		emptySceneTemplate.setBackgroundEnabled(game.getGamePreferences().isFirstBackgroundEnabled());
+
+		emptySceneTemplate.apply(worldWrapper);
+
+		worldWrapper.update(1);
 	}
 
 	private ControllerType[] getAvailableControllers() {
@@ -314,7 +269,7 @@ public class ControllerSettingsGameState extends GameStateImpl {
 		String pageView = "/settings/control/" + selectedControllerType.name().toLowerCase() + "/test";
 		Analytics.traker.trackPageView(pageView, pageView, null);
 		game.transition(Screens.ControllersTest) //
-				.parameter("controllerTest/backgroundEnabled", renderLayers.get(Layers.FirstBackground).isEnabled()) //
+				.parameter("controllerTest/backgroundEnabled", game.getGamePreferences().isFirstBackgroundEnabled()) //
 				.parameter("testControllerType", selectedControllerType) //
 				.start();
 	}
