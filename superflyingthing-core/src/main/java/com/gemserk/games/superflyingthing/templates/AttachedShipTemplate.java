e7b2d156be275ab51a6a799689b2acad06ff3e1c
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-27 19:10:11 -0300

    extracted AttachedShipTemplate from EntityTemplates class

---
+++ superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/templates/AttachedShipTemplate.java
@@ -0,0 +1,72 @@
+package com.gemserk.games.superflyingthing.templates;
+
+import com.artemis.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.gemserk.animation4j.gdx.Animation;
+import com.gemserk.commons.artemis.components.AnimationComponent;
+import com.gemserk.commons.artemis.components.PhysicsComponent;
+import com.gemserk.commons.artemis.components.RenderableComponent;
+import com.gemserk.commons.artemis.components.ScriptComponent;
+import com.gemserk.commons.artemis.components.SpatialComponent;
+import com.gemserk.commons.artemis.components.SpriteComponent;
+import com.gemserk.commons.artemis.components.TagComponent;
+import com.gemserk.commons.artemis.templates.EntityTemplateImpl;
+import com.gemserk.commons.gdx.box2d.BodyBuilder;
+import com.gemserk.commons.gdx.games.PhysicsImpl;
+import com.gemserk.commons.gdx.games.SpatialPhysicsImpl;
+import com.gemserk.games.superflyingthing.components.Components.AttachableComponent;
+import com.gemserk.games.superflyingthing.components.Components.MovementComponent;
+import com.gemserk.games.superflyingthing.scripts.Scripts;
+import com.gemserk.games.superflyingthing.templates.EntityTemplates.CategoryBits;
+import com.gemserk.resources.ResourceManager;
+
+public class AttachedShipTemplate extends EntityTemplateImpl {
+	
+	ResourceManager<String> resourceManager;
+	BodyBuilder bodyBuilder;
+
+	public AttachedShipTemplate() {
+		parameters.put("maxLinearSpeed", new Float(4f));
+		parameters.put("maxAngularVelocity", new Float(400f));
+	}
+
+	@Override
+	public void apply(Entity e) {
+		float width = 0.8f;
+		float height = 0.8f;
+
+		Animation rotationAnimation = resourceManager.getResourceValue("ShipAnimation");
+
+		Vector2 position = parameters.get("position");
+
+		Float maxLinearSpeed = parameters.get("maxLinearSpeed");
+		Float maxAngularVelocity = parameters.get("maxAngularVelocity");
+
+		Gdx.app.log("SuperFlyingThing", "Building new attached ship with " + maxLinearSpeed + ", " + maxAngularVelocity);
+
+		Body body = bodyBuilder //
+				.fixture(bodyBuilder.fixtureDefBuilder() //
+						.restitution(0f) //
+						.categoryBits(CategoryBits.ShipCategoryBits) //
+						.maskBits((short) 0) //
+						.boxShape(width * 0.125f, height * 0.125f)) //
+				.mass(50f) //
+				.position(position.x, position.y) //
+				.type(BodyType.DynamicBody) //
+				.userData(e) //
+				.build();
+
+		e.addComponent(new TagComponent(Groups.ship));
+		e.addComponent(new PhysicsComponent(new PhysicsImpl(body)));
+		e.addComponent(new SpatialComponent(new SpatialPhysicsImpl(body, width, height)));
+		e.addComponent(new SpriteComponent(rotationAnimation.getCurrentFrame()));
+		e.addComponent(new RenderableComponent(1));
+		e.addComponent(new MovementComponent(1f, 0f, maxLinearSpeed, maxAngularVelocity));
+		e.addComponent(new AttachableComponent());
+		e.addComponent(new ScriptComponent(new Scripts.AttachedShipScript()));
+		e.addComponent(new AnimationComponent(new Animation[] { rotationAnimation }));
+	}
+}
--- superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/templates/EntityTemplates.java
+++ superflyingthing-core/src/main/java/com/gemserk/games/superflyingthing/templates/EntityTemplates.java
@@ -1,7 +1,6 @@
 package com.gemserk.games.superflyingthing.templates;
 
 import com.artemis.Entity;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
@@ -61,51 +60,6 @@ import com.gemserk.games.superflyingthing.scripts.TimerScript;
 import com.gemserk.resources.ResourceManager;
 
 public class EntityTemplates {
-
-	private class AttachedShipTemplate extends EntityTemplateImpl {
-
-		public AttachedShipTemplate() {
-			parameters.put("maxLinearSpeed", new Float(4f));
-			parameters.put("maxAngularVelocity", new Float(400f));
-		}
-
-		@Override
-		public void apply(Entity e) {
-			float width = 0.8f;
-			float height = 0.8f;
-
-			Animation rotationAnimation = resourceManager.getResourceValue("ShipAnimation");
-
-			Vector2 position = parameters.get("position");
-
-			Float maxLinearSpeed = parameters.get("maxLinearSpeed");
-			Float maxAngularVelocity = parameters.get("maxAngularVelocity");
-
-			Gdx.app.log("SuperFlyingThing", "Building new attached ship with " + maxLinearSpeed + ", " + maxAngularVelocity);
-
-			Body body = bodyBuilder //
-					.fixture(bodyBuilder.fixtureDefBuilder() //
-							.restitution(0f) //
-							.categoryBits(CategoryBits.ShipCategoryBits) //
-							.maskBits((short) 0) //
-							.boxShape(width * 0.125f, height * 0.125f)) //
-					.mass(50f) //
-					.position(position.x, position.y) //
-					.type(BodyType.DynamicBody) //
-					.userData(e) //
-					.build();
-
-			e.addComponent(new TagComponent(Groups.ship));
-			e.addComponent(new PhysicsComponent(new PhysicsImpl(body)));
-			e.addComponent(new SpatialComponent(new SpatialPhysicsImpl(body, width, height)));
-			e.addComponent(new SpriteComponent(rotationAnimation.getCurrentFrame()));
-			e.addComponent(new RenderableComponent(1));
-			e.addComponent(new MovementComponent(1f, 0f, maxLinearSpeed, maxAngularVelocity));
-			e.addComponent(new AttachableComponent());
-			e.addComponent(new ScriptComponent(new Scripts.AttachedShipScript()));
-			e.addComponent(new AnimationComponent(new Animation[] { rotationAnimation }));
-		}
-	}
 
 	public static class CategoryBits {
 
@@ -208,6 +162,7 @@ public class EntityTemplates {
 		this.replayShipTemplate = templateProvider.get(ReplayShipTemplate.class);
 		this.laserBulletTemplate = templateProvider.get(LaserBulletTemplate.class);
 		this.laserGunTemplate = templateProvider.get(LaserGunTemplate.class);
+		this.attachedShipTemplate = templateProvider.get(AttachedShipTemplate.class);
 		
 	}
 
@@ -222,6 +177,8 @@ public class EntityTemplates {
 	
 	public EntityTemplate laserBulletTemplate;
 	public EntityTemplate laserGunTemplate;
+	
+	public EntityTemplate attachedShipTemplate;
 
 	public EntityTemplate particleEmitterTemplate = new EntityTemplateImpl() {
 
@@ -307,7 +264,7 @@ public class EntityTemplates {
 		}
 	};
 
-	private EntityTemplate attachedShipTemplate = new AttachedShipTemplate();
+	
 
 	private EntityTemplate replayPlayerTemplate = new EntityTemplateImpl() {
 
